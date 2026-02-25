9ae55f68bd2f2348c178412c90314ac250bfe43c
River City Cyber <191951124+RiverCityCyber@users.noreply.github.com>
2026-02-24 21:26:48 -0500

    Update and rename pacura.sh to webcam.sh

---
+++ webcam.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+find /ssd/videos -type f -name "*.mjpeg" -mtime +28 -delete
+ffmpeg -f v4l2 -input_format mjpeg -video_size 1280x720 -framerate 30 -i /dev/video0 -c:v copy -f segment -segment_time 60 -segment_atclocktime 1 -strftime 1 "dashcam_%Y%m%d_%H%M%S.mjpeg"
