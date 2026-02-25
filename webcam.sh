2c8afe637be6370fb89f89e2de3edeac37888a78
River City Cyber <191951124+RiverCityCyber@users.noreply.github.com>
2026-02-24 21:42:28 -0500

    Change video format from MJPEG to HEVC for output

--- webcam.sh
+++ webcam.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 find /mnt/ssd/videos -type f -name "*.mjpeg" -mtime +28 -delete
-ffmpeg -f v4l2 -input_format mjpeg -video_size 1280x720 -framerate 30 -i /dev/video0 -c:v copy -f segment -segment_time 60 -segment_atclocktime 1 -strftime 1 "dashcam_%Y%m%d_%H%M%S.mjpeg"
+ffmpeg -f v4l2 -input_format mjpeg -video_size 1280x720 -framerate 30 -i /dev/video0 -c:v hevc_v4l2m2m -f segment -segment_time 60 \
+-segment_atclocktime 1 -strftime 1 "/mnt/ssd/videos/dashcam_%Y%m%d_%H%M%S.mkv"
